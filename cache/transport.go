b4764cee5d0acca5a308982ae9094e5381ea37f3
Yutaka Ichibangase <yichiban@gmail.com>
2016-12-04 15:53:51 +0900

    cache only cacheable responses

--- cache/transport.go
+++ cache/transport.go
@@ -1,8 +1,10 @@
 package cache
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"regexp"
+	"strings"
 )
 
 type Transport struct {
@@ -25,16 +27,18 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 
 	resp := t.Get(req)
 	if resp != nil && valid(resp) {
-		log.Print("cache hit")
+		log.Printf("from cache: %s", req.URL)
 		return resp, nil
 	}
+
+	log.Printf("from backend: %s", req.URL)
 
 	resp, err := base.RoundTrip(req)
 	if err != nil {
 		return resp, err
 	}
 
-	if cacheable(resp) {
+	if cacheable(req, resp) {
 		t.Set(req, resp)
 	}
 
@@ -42,9 +46,61 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 }
 
 func valid(resp *http.Response) bool {
+	// TODO: implement
 	return resp != nil
 }
 
-func cacheable(resp *http.Response) bool {
-	return resp != nil
+// check if the req/resp pair is cacheable based on https://tools.ietf.org/html/rfc7234#section-3
+func cacheable(req *http.Request, resp *http.Response) bool {
+	if req.Method != http.MethodGet {
+		return false
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return false
+	}
+
+	if contains(req.Header, "Cache-Control", regexp.MustCompile(`\Ano-cache\z`)) {
+		return false
+	}
+
+	if contains(resp.Header, "Cache-Control", regexp.MustCompile(`\A(?:no-cache|private)\z`)) {
+		return false
+	}
+
+	if _, ok := req.Header["Authorization"]; ok {
+		if !contains(resp.Header, "Cache-Control", regexp.MustCompile(`\A(?:must-revalidate|public|s-maxage=\d+)\z`)) {
+			return false
+		}
+	}
+
+	if _, ok := resp.Header["Expires"]; ok {
+		return true
+	}
+
+	if contains(resp.Header, "Cache-Control", regexp.MustCompile(`\A(?:maxage=\d+|s-maxage=\d+|public)\z`)) {
+		return true
+	}
+
+	return false
+}
+
+func contains(h http.Header, key string, value *regexp.Regexp) bool {
+	vs, ok := h[key]
+	if !ok {
+		return false
+	}
+
+	for _, v := range vs {
+		vs := strings.Split(v, ",")
+
+		for _, v := range vs {
+			v := strings.Trim(v, " ")
+			if value.MatchString(v) {
+				return true
+			}
+		}
+	}
+
+	return false
 }
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 
-	"github.com/ichiban/jesi/embed"
 	"github.com/ichiban/jesi/cache"
+	"github.com/ichiban/jesi/embed"
 )
 
 func main() {
 	addr := ":8080"
-	uri, _ := url.Parse("http://127.0.0.1:3000")
+	uri, err := url.Parse("http://127.0.0.1:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
 	handler := httputil.NewSingleHostReverseProxy(uri)
 	handler.Transport = &embed.Transport{&cache.Transport{}}
 	server := http.Server{

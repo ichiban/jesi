bcc44a3926ec35b52ca322c02c39493b41c7dd44
Yutaka Ichibangase <yichiban@gmail.com>
2017-11-04 13:27:15 +0900

    embedding spec in HTTP header

--- embed/handler_test.go
+++ embed/handler_test.go
@@ -5,27 +5,40 @@ import (
 	"testing"
 
 	"github.com/ichiban/jesi/cache"
+	"net/url"
 )
 
 func TestHandler_ServeHTTP(t *testing.T) {
 	testCases := []struct {
-		url       string
+		req       *http.Request
 		resources map[string]*testResource
-		header    http.Header
-		body      string
+		resp      *cache.Representation
 	}{
 		{ // without 'with' query parameter, it simply returns JSON.
-			url: "/test",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path: "/test",
+				},
+			},
 			resources: map[string]*testResource{
 				"/test": {
 					header: http.Header{"Content-Type": []string{"application/json"}},
 					body:   `{}`,
 				},
 			},
-			body: `{}`,
+			resp: &cache.Representation{
+				Body: []byte(`{}`),
+			},
 		},
 		{ // with 'with' query parameter, it embeds resources specified by edges.
-			url: "/a?with=foo.bar.baz",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path:     "/a",
+					RawQuery: "with=foo.bar.baz",
+				},
+			},
 			resources: map[string]*testResource{
 				"/a": {
 					header: http.Header{"Content-Type": []string{"application/json"}},
@@ -40,10 +53,18 @@ func TestHandler_ServeHTTP(t *testing.T) {
 					body:   `{"_links":{"next":{"href":"/a"},"self":{"href":"/c"}}}`,
 				},
 			},
-			body: `{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{},"_links":{"next":{"href":"/a"},"self":{"href":"/c"}}}},"_links":{"bar":{"href":"/c"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
+			resp: &cache.Representation{
+				Body: []byte(`{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{},"_links":{"next":{"href":"/a"},"self":{"href":"/c"}}}},"_links":{"bar":{"href":"/c"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`),
+			},
 		},
 		{ // multiple 'with' query parameters are also fine.
-			url: "/a?with=foo.bar.baz&with=foo.qux.quux",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path:     "/a",
+					RawQuery: "with=foo.bar.baz&with=foo.qux.quux",
+				},
+			},
 			resources: map[string]*testResource{
 				"/a": {
 					header: http.Header{"Content-Type": []string{"application/json"}},
@@ -66,30 +87,127 @@ func TestHandler_ServeHTTP(t *testing.T) {
 					body:   `{"_links":{"corge":{"href":"/a"},"self":{"href":"/e"}}}`,
 				},
 			},
-			body: `{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{"baz":{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}},"_links":{"baz":{"href":"/a"},"self":{"href":"/c"}}},"qux":{"_embedded":{"quux":{"_links":{"corge":{"href":"/a"},"self":{"href":"/e"}}}},"_links":{"quux":{"href":"/e"},"self":{"href":"/d"}}}},"_links":{"bar":{"href":"/c"},"qux":{"href":"/d"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
+			resp: &cache.Representation{
+				Body: []byte(`{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{"baz":{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}},"_links":{"baz":{"href":"/a"},"self":{"href":"/c"}}},"qux":{"_embedded":{"quux":{"_links":{"corge":{"href":"/a"},"self":{"href":"/e"}}}},"_links":{"quux":{"href":"/e"},"self":{"href":"/d"}}}},"_links":{"bar":{"href":"/c"},"qux":{"href":"/d"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`),
+			},
+		},
+		{ // even With header fields do.
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path: "/a",
+				},
+				Header: http.Header{
+					"With": []string{"foo.bar.baz", "foo.qux.quux"},
+				},
+			},
+			resources: map[string]*testResource{
+				"/a": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
+				},
+				"/b": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"bar":{"href":"/c"},"qux":{"href":"/d"},"self":{"href":"/b"}}}`,
+				},
+				"/c": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"baz":{"href":"/a"},"self":{"href":"/c"}}}`,
+				},
+				"/d": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"quux":{"href":"/e"},"self":{"href":"/d"}}}`,
+				},
+				"/e": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"corge":{"href":"/a"},"self":{"href":"/e"}}}`,
+				},
+			},
+			resp: &cache.Representation{
+				Body: []byte(`{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{"baz":{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}},"_links":{"baz":{"href":"/a"},"self":{"href":"/c"}}},"qux":{"_embedded":{"quux":{"_links":{"corge":{"href":"/a"},"self":{"href":"/e"}}}},"_links":{"quux":{"href":"/e"},"self":{"href":"/d"}}}},"_links":{"bar":{"href":"/c"},"qux":{"href":"/d"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`),
+			},
+		},
+		{ // or mixture of query string and With header field.
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path:     "/a",
+					RawQuery: "with=foo.bar.baz",
+				},
+				Header: http.Header{
+					"With": []string{"foo.qux.quux"},
+				},
+			},
+			resources: map[string]*testResource{
+				"/a": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
+				},
+				"/b": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"bar":{"href":"/c"},"qux":{"href":"/d"},"self":{"href":"/b"}}}`,
+				},
+				"/c": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"baz":{"href":"/a"},"self":{"href":"/c"}}}`,
+				},
+				"/d": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"quux":{"href":"/e"},"self":{"href":"/d"}}}`,
+				},
+				"/e": {
+					header: http.Header{"Content-Type": []string{"application/json"}},
+					body:   `{"_links":{"corge":{"href":"/a"},"self":{"href":"/e"}}}`,
+				},
+			},
+			resp: &cache.Representation{
+				Body: []byte(`{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{"baz":{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}},"_links":{"baz":{"href":"/a"},"self":{"href":"/c"}}},"qux":{"_embedded":{"quux":{"_links":{"corge":{"href":"/a"},"self":{"href":"/e"}}}},"_links":{"quux":{"href":"/e"},"self":{"href":"/d"}}}},"_links":{"bar":{"href":"/c"},"qux":{"href":"/d"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`),
+			},
 		},
 		{ // if the response is not JSON, it simply returns the response.
-			url: "/a?with=foo.bar.baz",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path:     "/a",
+					RawQuery: "with=foo.bar.baz",
+				},
+			},
 			resources: map[string]*testResource{
 				"/a": {
 					header: http.Header{"Content-Type": []string{"application/xml"}},
 					body:   `{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
 				},
 			},
-			body: `{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
+			resp: &cache.Representation{
+				Body: []byte(`{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`),
+			},
 		},
 		{ // if the specified edge is not found, it embeds a corresponding error document JSON.
-			url: "/a?with=foo",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path:     "/a",
+					RawQuery: "with=foo",
+				},
+			},
 			resources: map[string]*testResource{
 				"/a": {
 					header: http.Header{"Content-Type": []string{"application/json"}},
 					body:   `{"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
 				},
 			},
-			body: `{"_embedded":{"foo":{"type":"https://ichiban.github.io/jesi/problems/response-error","title":"Response Error","status":404,"detail":"Not Found","_links":{"about":"/b"}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
+			resp: &cache.Representation{
+				Body: []byte(`{"_embedded":{"foo":{"type":"https://ichiban.github.io/jesi/problems/response-error","title":"Response Error","status":404,"detail":"Not Found","_links":{"about":"/b"}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`),
+			},
 		},
 		{ // the resulting Cache-Control is the weakest of all.
-			url: "/a?with=foo.bar.baz",
+			req: &http.Request{
+				Method: http.MethodGet,
+				URL: &url.URL{
+					Path:     "/a",
+					RawQuery: "with=foo.bar.baz",
+				},
+			},
 			resources: map[string]*testResource{
 				"/a": {
 					header: http.Header{
@@ -113,20 +231,17 @@ func TestHandler_ServeHTTP(t *testing.T) {
 					body: `{"_links":{"next":{"href":"/a"},"self":{"href":"/c"}}}`,
 				},
 			},
-			header: http.Header{
-				"Content-Type":  []string{"application/json"},
-				"Cache-Control": []string{"private,max-age=10"},
+			resp: &cache.Representation{
+				HeaderMap: http.Header{
+					"Content-Type":  []string{"application/json"},
+					"Cache-Control": []string{"private,max-age=10"},
+				},
+				Body: []byte(`{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{},"_links":{"next":{"href":"/a"},"self":{"href":"/c"}}}},"_links":{"bar":{"href":"/c"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`),
 			},
-			body: `{"_embedded":{"foo":{"_embedded":{"bar":{"_embedded":{},"_links":{"next":{"href":"/a"},"self":{"href":"/c"}}}},"_links":{"bar":{"href":"/c"},"self":{"href":"/b"}}}},"_links":{"foo":{"href":"/b"},"self":{"href":"/a"}}}`,
 		},
 	}
 
 	for i, tc := range testCases {
-		req, err := http.NewRequest(http.MethodGet, tc.url, nil)
-		if err != nil {
-			t.Errorf("(%d) err is not nil: %v", i, err)
-		}
-
 		th := &testHandler{
 			T:         t,
 			Resources: tc.resources,
@@ -134,13 +249,13 @@ func TestHandler_ServeHTTP(t *testing.T) {
 		e := Handler{Next: th}
 
 		var rep cache.Representation
-		e.ServeHTTP(&rep, req)
+		e.ServeHTTP(&rep, tc.req)
 
 		if http.StatusOK != rep.StatusCode {
-			t.Errorf("(%d) expected 200, got %d, %s", i, rep.StatusCode, req.URL)
+			t.Errorf("(%d) expected 200, got %d, %s", i, rep.StatusCode, tc.req.URL)
 		}
 
-		for k, vs := range tc.header {
+		for k, vs := range tc.resp.HeaderMap {
 			for i, v := range vs {
 				if v != rep.HeaderMap[k][i] {
 					t.Errorf("(%d) (%s) expected %s, got %s", i, k, v, rep.HeaderMap[k][i])
@@ -149,8 +264,8 @@ func TestHandler_ServeHTTP(t *testing.T) {
 			}
 		}
 
-		if tc.body != string(rep.Body) {
-			t.Errorf("(%d) expected: %s, got: %s", i, tc.body, string(rep.Body))
+		if string(tc.resp.Body) != string(rep.Body) {
+			t.Errorf("(%d) expected: %s, got: %s", i, string(tc.resp.Body), string(rep.Body))
 		}
 	}
 }
